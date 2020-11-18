37d3fe893e8ce508c3b99c158ce982856d52cc44
Tloz <ScrimeT@hotmail.fr>
2020-11-18 21:11:11 +0100

    added version function

--- main.cpp
+++ main.cpp
@@ -7,21 +7,34 @@ using namespace std;
 #define SEMVER_y 0
 #define SEMVER_z 1
 
+void usage()
+{
+
+}
+
+void version()
+{
+    // Use this if you want to add detail on SEMVER
+    // Using this cuz we cant leave an empty #define
+    // Yes, I know, that's ugly
+    string SEMVER_addendum("");
+
+    cout << "libspu v" << SEMVER_x << "." << SEMVER_y << "." << SEMVER_z;
+    if(SEMVER_addendum.length() != 0)
+    {
+        cout << SEMVER_addendum;
+    }
+    cout << " (using SEMVER)" << endl;
+    cout << "Compiled on " << __DATE__ << endl;
+    return 0;
+}
+
 
 int main(int argc, char const *argv[])
 {
-    string SEMVER_addendum("");
-
     if((argc >= 1) && (string(argv[1]).compare(string("-v")) == 0))
     {
-        cout << "libspu v" << SEMVER_x << "." << SEMVER_y << "." << SEMVER_z;
-        if(SEMVER_addendum.length() != 0)
-        {
-            cout << SEMVER_addendum;
-        }
-        cout << endl << "(using SEMVER)" << endl;
-        cout << "Compiled on " << __DATE__ << endl;
-        // quit after printing
+        version()
         return 0;
     }
     return 0;
