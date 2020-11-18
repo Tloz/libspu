6d13c36fd75c012e71a1c637031d34bc93c073b6
Tloz <ScrimeT@hotmail.fr>
2020-11-18 21:31:52 +0100

    add random branch

---
+++ inc/random.h
@@ -0,0 +1,16 @@
+#ifndef RANDOM_H
+#define RANDOM_H
+
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <cstdlib>
+// #include <vector>
+// #include <string>
+
+using namespace std;
+
+float randFloatInRange(float min, float max);
+
+
+#endif // RANDOM_H
