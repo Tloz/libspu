9a69e407ae3c5da04d88bd41f4279da78362c7a6
Tloz <ScrimeT@hotmail.fr>
2020-11-19 15:04:35 +0100

    revamp RNG

--- src/random.cpp
+++ src/random.cpp
@@ -1,13 +1,21 @@
 #include "../inc/random.h"
 
-#include <iostream>
-#include <iomanip>
-#include <sstream>
-#include <cstdlib>
-// #include <vector>
-// #include <string>
+// #include <iostream>
+// #include <iomanip>
+// #include <sstream>
 
-float libpsu::randFloatInRange(float min, float max)
+
+int libspu::random_Int(int min, int max)
+{
+    return min + rand() % max;
+}
+
+float libspu::random_Float()
+{
+    return rand();
+}
+
+float libspu::random_Float(float min, float max)
 {
     return min + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (max - min)));
 }
