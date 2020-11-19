fc10c9778af159823441b7c770a4d01bcba4d3fc
Tloz <ScrimeT@hotmail.fr>
2020-11-19 17:45:07 +0100

    handle version info in a separate file

---
+++ inc/version.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+#define SPU_SEMVER_x 0
+#define SPU_SEMVER_y 0
+#define SPU_SEMVER_z 1
+
+using namespace std;
+
+namespace libspu
+{
+    int version_x();
+    int version_y();
+    int version_z();
+    string version_a();
+    string version();
+    string compiledate();
+    void version_info();
+}
