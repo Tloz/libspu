461d07714493bdef2d5fef52e288597b3ac46ce4
Tloz <ScrimeT@hotmail.fr>
2020-11-20 13:06:43 +0100

    this is why you name your variables with meaningfull names and comment your code

--- inc/2darray.h
+++ inc/2darray.h
@@ -4,24 +4,54 @@ namespace libspu
     {
     private:
         int** m_array;
-        int m_col;
-        int m_row;
+        int m_width;
+        int m_height;
 
     public:
+        // ctors and dtors
+        // this one is empty, do not use
         TDArray();
-        TDArray(int col, int row, int defaultValue = 0);
+        // use this one
+        TDArray(int width, int height, int defaultValue = 0);
         ~TDArray();
+
+        // returns the number of cells in tab
         int cardinal() const;
-        void display() const;
-        int row() const;
-        int col() const;
+
+        // displays the tab, fancy will show indexes
+        void display(bool fancy = false) const;
+
+        // returns width of the tab
+        int width() const;
+
+        // returns height of the tab
+        int height() const;
+
+        // return all the numbers in row index
         int* row(int index) const;
+
+        // return all the numbers in column index
         int* col(int index) const;
-        int at(int c, int r, bool top = false) const;
-        void set(int c, int r, int value, bool top = false);
+
+        // returns the number at (x;y) set top to true if you want to input 
+        // your y value from the top (like a programmer)
+        // or from the bottom (like a normal human beeing)
+        int at(int x, int y, bool top = false) const;
+        
+        // replace current value at (x;y) with newVal
+        // top is the same as above
+        void set(int x, int y, int newVal, bool top = false);
+        
+        // returns the sum of all cells
         int sum() const;
+        
+        // returns the average value of all cells
         float average() const;
+        
+        // return the sum of all cells on given row
         int sumOnRow(int index) const;
+        
+        // return the sum of all cells on given column
         int sumOnCol(int index) const;
     };
 }
