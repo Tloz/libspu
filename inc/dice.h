e4231c6a7a8c3e639791bc526d6f608013bc548c
Tloz <scrimet@hotmail.fr>
2021-11-17 22:15:34 +0100

    created dice module

---
+++ inc/dice.h
@@ -0,0 +1,32 @@
+#ifndef DICE_H
+#define DICE_H
+
+/***************************************************************************
+*             Dice module for SimpleProjectUtilities
+*
+*   File    : dice.h
+*   Author  : Emeric Frémion <ScrimeT@hotmail.fr>
+*   Date    : November 17, 2021
+*   Licence : The Unlicence - https://unlicense.org/
+*   Repo    : https://github.com/Tloz/libspu
+*   Purpose : Header for dice throwing functions.
+*             Part of the SimpleProjectUtilities library
+*
+***************************************************************************/
+
+/**************************************************************************\
+*                  REQUIRED HEADERS AND MACRO DEFINITION                   *
+\**************************************************************************/
+
+#include "random.h"
+
+/**************************************************************************\
+*                               PROTOTYPES                                 *
+\**************************************************************************/
+
+namespace libspu
+{
+    
+}
+
+#endif // DICE_H /***************** END OF FILE - dice.h ******************/
